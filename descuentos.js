2aa046154383e07bd821d8b7f5e58f1c9e74f985
Ivan Flores <ivanpaulfloresgu15@gmail.com>
2021-07-23 04:32:48 -0500

    Taller 2 completo

---
+++ descuentos.js
@@ -0,0 +1,40 @@
+function formulaDescuento(precio, descuento){
+  const porcentajePrecioConDescuento = 100 - descuento;
+  const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
+
+  return precioConDescuento;
+}
+
+const verificarCupon = function () {
+  const cupones = [10, 25, 50];
+  const cupon = document.getElementById("cupones");
+  const valueCupon = parseInt(cupon.value);
+  if (valueCupon === cupones[0]) {
+    return cupones[0];
+  } else if (valueCupon === cupones[1]) {
+    return cupones[1];
+  } else if (valueCupon === cupones[2]) {
+    return cupones[2];
+  } else {
+    return 0;
+  }
+};
+
+function calcularPrecioConDescuento() {
+  const inputPrice = document.getElementById("InputPrice");
+  const priceValue = parseInt(inputPrice.value);
+
+  const InputDiscount = document.getElementById("InputDiscount");
+  const discountValue = parseInt(InputDiscount.value);
+
+  const verificacionCupon = verificarCupon();
+  const descuentoCupon = discountValue + verificacionCupon;
+
+  const precioConDescuento = formulaDescuento(priceValue, descuentoCupon);
+
+  const resultDescuento = document.getElementById("resultDescuento");
+  resultDescuento.innerText = "El precio con descuento es: S/. " + precioConDescuento;
+
+  
+}
+
